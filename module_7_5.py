385149ffde7c85d6804af8d7e33610b80cd71e38
Serj456 <121343943+Serj456@users.noreply.github.com>
2024-12-02 17:22:34 +0300

    Update module_7_5.py

--- module_7_5.py
+++ module_7_5.py
@@ -3,7 +3,8 @@ import time
 directory = '.'
 for root, dirs, files in os.walk(directory):
   for file in files:
-    filepath = os.path.join(file)
+
+    filepath = os.path.join(os.getcwd(), file)
     filetime = os.path.getmtime(file)
     formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
     filesize = os.path.getsize(file)
